64502342a220a3ce3b1dfb555f25168deb8c9452
Nikita Isakov <neta782278@yandex.ru>
2023-05-22 22:15:03 +0700

    comit

---
+++ main.py
@@ -0,0 +1,36 @@
+# def orbit_hight(list_1: list)->tuple:
+#     dict_1={round(orbit[0]*orbit[1]): orbit for orbit in filter(lambda orbit: orbit[0]!=orbit[1], list_1)} #нихуя не понял но очень интересно
+#     return max(dict_1, key=lambda k:dict_1[k]), dict_1[max(dict_1)]
+# list_2=[(1,3), (1,34), (123,3), (1123,3), (111,3), (133,3)]
+# print(orbit_hight(list_2))
+
+
+
+
+
+# Напишите функцию same_by(characteristic, objects), которая
+# проверяет, все ли объекты имеют одинаковое значение
+# некоторой характеристики, и возвращают True, если это так.
+# Если значение характеристики для разных объектов
+# отличается - то False. Для пустого набора объектов, функция
+# должна возвращать True. Аргумент characteristic - это
+# функция, которая принимает объект и вычисляет его
+# характеристику.
+
+# жоска как-то, не вкурил совсем
+# list_1=[0, 2, 4, 8]
+# list_2=[]
+# def function_1(char, obj):
+#     list_2=set(list(map(char, obj)))
+#     if len(list_2)>1:
+#         print("False")
+#     else:
+#         print("True")
+# char=function_1(lambda x: x%2==0, list_1)
+
+
+
+
+# HOMEWORK
+
+
